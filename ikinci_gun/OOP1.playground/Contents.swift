5107b99f3164cecd2d7acab508b11f8c0fc4c7cf
Ayşe Beyza Gür <beyyzgur@gmail.com>
2025-06-11 01:10:32 +0300

    feat: update OOP1 for learning functions & static

--- ikinci_gun/OOP1.playground/Contents.swift
+++ ikinci_gun/OOP1.playground/Contents.swift
@@ -72,5 +72,79 @@ var sahin = Araba()
 sahin.renk = "Kırmızı"
 sahin.hiz = 0
 sahin.calisiyorMu = false
-
 sahin.bilgiAl()
+
+
+class Fonksiyonlar {
+    //Geri dönüş deeğeri olmayan  ( void )
+    func selamla1() {
+        let word = "Merhaba"
+        print(word)
+    }
+    
+    //Geri dönüş deeğeri olmayan  ( return )
+    func selamla2() -> String { // geri dönüş değerinin type'i iste
+        let word = "return var sende canim"
+        return word
+    }
+    
+    //Parametre
+    func selamla3(isim:String) {
+        let sonuc = "Merhaba \(isim)"
+        print(sonuc)
+    }
+    
+    func toplama(sayi1:Int, sayi2:Int) -> Int {
+        let sonuc = sayi1 + sayi2
+        return sonuc
+    }
+    
+    //Overloading
+    
+    func carpma(sayi1:Int, sayi2:Int) -> Int {
+        let sonuc = sayi1 * sayi2
+        return sonuc
+    }
+    
+    func carpma(sayi1:Double, sayi2:Double) -> Double {
+        let sonuc = sayi1 * sayi2
+        return sonuc
+    }
+    
+    func carpma(sayi1:Int, sayi2:Int, sayi3:Int) -> Int {
+        let sonuc = sayi1 * sayi2 * sayi3
+        return sonuc
+    }
+}
+
+let f = Fonksiyonlar()
+
+f.selamla1()
+f.selamla2()
+f.selamla3(isim: "Beyzoş")
+let toplamSonucu = f.toplama(sayi1: 8, sayi2: 12)
+print("Toplam Sonucu : \(toplamSonucu)")
+let ilkCarpmaSonucu = f.carpma(sayi1: 2, sayi2: 7)
+let ikinciCarpmaSonucunu = f.carpma(sayi1: 2, sayi2: 7, sayi3: 3)
+print("İlk Carpma Sonucu : \(ilkCarpmaSonucu)")
+print("İkinci Carpma Sonucu : \(ikinciCarpmaSonucunu)")
+
+//Statik Değişkenler ve Metodlar
+
+class ASinifi {
+    static let x = 10 // var diyince kırmızı hata verdi niyeyse
+    
+    static func metod() {
+        print("Metod çağırıldı.")
+    }
+}
+/*
+let a = ASinifi()
+print(a.x)
+a.metod()
+
+print(ASinifi().x) // Sanal Nesne - Virtual Object - İsimsiz Nesne
+ASinifi().metod() // Bunu yapma, boşuna hafızada 2 nesne oluşturuyor. Yukarıda let a diyerek hafızada sadece 1 adet nesne oluşturmuştuk.
+*/
+print(ASinifi.x)
+ASinifi.metod()
