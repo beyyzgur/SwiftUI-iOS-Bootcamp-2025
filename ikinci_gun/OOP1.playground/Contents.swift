78822084d09c0562b34e69cbd8828607a7508334
Ayşe Beyza Gür <beyyzgur@gmail.com>
2025-06-20 19:11:18 +0300

    feat: update OOP1 for learn enumeration & composition

--- ikinci_gun/OOP1.playground/Contents.swift
+++ ikinci_gun/OOP1.playground/Contents.swift
@@ -148,3 +148,73 @@ ASinifi().metod() // Bunu yapma, boşuna hafızada 2 nesne oluşturuyor. Yukarı
 */
 print(ASinifi.x)
 ASinifi.metod()
+
+// Enumeration
+
+enum KonserveBoyut {
+    case kucuk
+    case orta
+    case buyuk
+}
+
+func ucretHesapla(boyut: KonserveBoyut, adet: Int) {
+    switch boyut {
+        case KonserveBoyut.kucuk: print("Fiyat: \(adet * 10)₺")
+        case .orta: print("Fİyat: \(adet * 10)")
+        case .buyuk: print("Fiyat: \(adet * 30)")
+    }
+}
+
+ucretHesapla(boyut: KonserveBoyut.buyuk, adet: 5)
+ucretHesapla(boyut: .kucuk, adet: 5)
+
+// Composition
+
+class Kategoriler {
+    var kategori_id:Int?
+    var kategori_ad:String?
+    
+    init(kategori_id:Int, kategori_ad:String) {
+        self.kategori_id = kategori_id
+        self.kategori_ad = kategori_ad
+    }
+}
+
+class Yonetmenler {
+    var yonetmen_id:Int?
+    var yonetmen_ad:String?
+    
+    init(yonetmen_id:Int, yonetmen_ad:String) {
+        self.yonetmen_id = yonetmen_id
+        self.yonetmen_ad = yonetmen_ad
+    }
+}
+
+class Filmler {
+    var film_id:Int?
+    var film_ad:String?
+    var film_yil:Int?
+    var kategori: Kategoriler?
+    var yonetmen: Yonetmenler?
+    
+    init(film_id:Int, film_ad:String, film_yil:Int, kategori: Kategoriler, yonetmen: Yonetmenler) {
+        self.film_id = film_id
+        self.film_ad = film_ad
+        self.film_yil = film_yil
+        self.kategori = kategori
+        self.yonetmen = yonetmen
+    }
+}
+
+let k1 = Kategoriler(kategori_id: 1, kategori_ad: "Dram")
+let k2 = Kategoriler(kategori_id: 2, kategori_ad: "Komedi")
+
+let y1 = Yonetmenler(yonetmen_id: 1, yonetmen_ad: "Quentin Tarantino")
+let y2 = Yonetmenler(yonetmen_id: 2, yonetmen_ad: "Christopher Nolan")
+
+let f1 = Filmler(film_id: 1, film_ad: "Django", film_yil: 2012, kategori: k1, yonetmen: y1)
+print("Film id: \(f1.film_id!)")
+print("Film ad: \(f1.film_ad!)")
+print("Film yıl: \(f1.film_yil!)")
+print("Film kategori: \(f1.kategori!.kategori_ad!)")
+print("Film yönetmen: \(f1.yonetmen!.yonetmen_ad!)")
